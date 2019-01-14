728e10de7d4a9de9a683f6c9122084bc744e160a
bkindzierski <bkindzierski@me.com>
2019-01-14 15:54:42 -0500

    updates to pdf writer

---
+++ PdfManager/PDFManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace PdfManager
+{
+    public class PDFFileManager
+    {
+        public PDFFileManager() {
+
+        }
+
+
+    }
+}
